a1fc005b05ce7cc053b6015b4bbf5d3db59a8d48
Sacha <Sacha.bigou@gmail.com>
2023-01-25 16:39:19 +0100

    fin de journée

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ var bookingsRouter = require("./routes/bookings");
 const { application } = require("express");
 
 var app = express();
-
+var cors = require("cors");
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
